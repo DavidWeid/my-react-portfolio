d524056ae18f14155f2313a4c187e6b667ee60f4
David Weid <david.weid.2@gmail.com>
2019-08-07 14:20:29 -0400

    Updates

---
+++ precache-manifest.0931bb8ab70df1b51d3f469eb7adcbed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d7c3c124e3cd25c9f312da15111f888",
+    "url": "/my-react-portfolio/index.html"
+  },
+  {
+    "revision": "1b90e189378f9eea8501",
+    "url": "/my-react-portfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/my-react-portfolio/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "1b90e189378f9eea8501",
+    "url": "/my-react-portfolio/static/js/main.41605cce.chunk.js"
+  },
+  {
+    "revision": "1fe5a7777cf2ea3dc2f2",
+    "url": "/my-react-portfolio/static/js/runtime~main.11e831a5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-react-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
